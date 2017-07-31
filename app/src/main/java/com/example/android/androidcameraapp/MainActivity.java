10ee7f0fbabb381ae67ab48d5bae3fd80039354c
DiegoE1 <fred.espinosa@g.austincc.edu>
2017-07-31 14:34:12 -0500

    Added code to try and add the picture above the button

--- app/src/main/java/com/example/android/androidcameraapp/MainActivity.java
+++ app/src/main/java/com/example/android/androidcameraapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.android.androidcameraapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mTakePicture;
 
+    private ImageView mImageView;
+    private String mCurrentPhotoPath;
+    private Bitmap mImageBitmap;
     private final int REQUEST_IMAGE_CAPTURE = 1;
 
 
@@ -26,20 +38,55 @@ public class MainActivity extends AppCompatActivity {
 
         mTakePicture = (Button) findViewById(R.id.button_take_picture);
 
+        mImageView = (ImageView) findViewById(R.id.image_view_picture);
+
         mTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+                    File photoFile = null;
+                    try{
+                        photoFile = createImageFile();
+                    } catch (IOException ex){
 
+                    }
+                    if(photoFile != null) {
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
             }
         });
     }
 
-    public void dispatchTakePictureIntent(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            try{
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+                mImageView.setImageBitmap(mImageBitmap);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*
+    public void openGallery(){
+        Intent i = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        final int ACTIVITY_SELECT_IMAGE = 1234;
+        startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+    }
+    */
 }
