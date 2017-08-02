1734821ddc3fdbdaae6ce95600d4c76dd7015b92
DiegoE1 <fred.espinosa@g.austincc.edu>
2017-08-02 14:45:52 -0500

    Added Permission to Camera when Button is pressed

--- app/src/main/java/com/example/android/androidcameraapp/MainActivity.java
+++ app/src/main/java/com/example/android/androidcameraapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.android.androidcameraapp;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -22,7 +27,9 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 2;
 
     // TODO: Create an app that can take pictures using Intents
     // todo:    You should have 1 Button that starts the camera
@@ -35,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mImageView;
     private File destination;
     private final int REQUEST_IMAGE_CAPTURE = 1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,45 +56,38 @@ public class MainActivity extends AppCompatActivity {
         mTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                takePictureIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-                    File photoFile = null;
-                    try{
-                        photoFile = createImageFile();
-                    } catch (IOException ex){
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)) {
+                        //explanation
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
                     }
-                    if(photoFile != null) {
-                        try {
-                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(destination));
-                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                        } catch (ActivityNotFoundException e){
-                            e.printStackTrace();
-                        }
-                    }
+                } else {
+                    takePicture();
                 }
+
+
             }
         });
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            try{
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
                 FileInputStream in = new FileInputStream(destination);
                 BitmapFactory.Options options = new BitmapFactory.Options();
                 options.inSampleSize = 10;
                 Bitmap userImage = BitmapFactory.decodeStream(in, null, options);
                 mImageView.setImageBitmap(userImage);
                 updateGallery(destination.getAbsolutePath(), MainActivity.this);
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private File createImageFile() throws IOException{
+    private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + ".jpg";
         destination = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), imageFileName);
@@ -97,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void updateGallery(String filePath, Context context){
+    private void updateGallery(String filePath, Context context) {
         ContentValues values = new ContentValues();
 
         values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
@@ -108,4 +107,36 @@ public class MainActivity extends AppCompatActivity {
         context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                }
+            }
+            return;
+        }
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                try {
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(destination));
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } catch (ActivityNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
